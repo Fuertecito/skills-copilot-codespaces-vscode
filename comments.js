ec954aed66e8f467ee01bb95ed93838b482531ce
Fuertecito <130132540+Fuertecito@users.noreply.github.com>
2023-11-01 23:12:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,17 @@
 //create a web server
-const express = require('express');
-//create a more complex web server
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
+var Post = require('../models/post');
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var config = require('../config/database');
+var passport = require('passport');
+require('../config/passport')(passport);
+
+  
+
+
 
 
 
